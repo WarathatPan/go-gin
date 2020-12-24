e51e524665d5b46ce93dfaa3a64fa85e2c7308a5
Warathat_panthong <warathat.pan@gmail.com>
2020-12-24 12:14:53 +0700

    add articles slice and test http get

--- main.go
+++ main.go
@@ -3,8 +3,6 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,21 +17,7 @@ func main() {
 	router.LoadHTMLGlob("templates/*")
 
 	// Initialize the routes
-	router.GET("/", func(c *gin.Context) {
-
-		// Call the HTML method of the Context to render a template
-		c.HTML(
-			// Set the HTTP status to 200 (OK)
-			http.StatusOK,
-			// Use the index.html template
-			"index.html",
-			// Pass the data that the page uses (in this case, 'title')
-			gin.H{
-				"title": "Home Page",
-			},
-		)
-
-	})
+	initializeRoutes()
 
 	// Start serving the application
 	router.Run()
